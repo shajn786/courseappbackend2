79999236116b40b58af7ebec544f78d95bed9a42
shajn786 <shajn786@gmail.com>
2022-12-06 11:15:25 +0530

    add course

--- src/main/java/com/example/course_backend_app/controller/CourseController.java
+++ src/main/java/com/example/course_backend_app/controller/CourseController.java
@@ -1,7 +1,15 @@
 package com.example.course_backend_app.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+
+
+    @PostMapping("/add")
+    public  String AddCouse()
+    {
+        return "added";
+    }
 }
