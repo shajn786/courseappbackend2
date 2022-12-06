bb085039d3eae12ba7d0c3f5a926423ded2185d8
shajn786 <shajn786@gmail.com>
2022-12-06 11:12:33 +0530

    initial

---
+++ src/main/java/com/example/course_backend_app/CourseBackendAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.course_backend_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBackendAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBackendAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/course_backend_app/CourseBackendAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course_backend_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBackendAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
